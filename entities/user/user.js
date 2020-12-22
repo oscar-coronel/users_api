38177729f41dd12ff680f168d5958e0a897531f2
Oscar Coronel <ocoronel31@gmail.com>
2020-12-21 22:12:32 -0500

    Añadiendo columna Resultado

--- entities/user/user.js
+++ entities/user/user.js
@@ -2,8 +2,8 @@ var conn = require('./../../databases/mysql/conn.js')
 
 var table = 'usuarios'
 
-var fillable = ['Nombres', 'Apellidos', 'Cedula', 'Email', 'Telefono']
-var requiredFields = ['Nombres', 'Apellidos', 'Cedula', 'Email', 'Telefono']
+var fillable = ['Nombres', 'Apellidos', 'Cedula', 'Email', 'Telefono', 'Resultado']
+var requiredFields = ['Nombres', 'Apellidos', 'Cedula', 'Email', 'Telefono', 'Resultado']
 
 async function get(){
     return await conn.get( table )
